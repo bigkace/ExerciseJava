f296fa32e05c27d6ac1c37693618bfeb5ef9f8e4
BigK_ACE <91551238+bigkace@users.noreply.github.com>
2021-12-02 11:35:10 +0700

    Add files via upload

---
+++ XuLy01.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class XuLy01 {
+
+	public XuLy01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int n = nhapN(scan);
+		int sum = recursionCalSum(n);
+		System.out.println("Tổng = " + sum);
+
+	}
+	
+	public static int nhapN(Scanner scan) {
+		int n;
+		do {
+			System.out.println("Nhập giá trị n > 0: ");
+			n = Integer.parseInt(scan.nextLine());
+		} while (n < 1);
+		return n;
+	}
+	
+	public static int recursionCalSum(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		return n + recursionCalSum(n - 1);
+	}
+
+}
